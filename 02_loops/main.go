8bc0b00a5e08c1c8a7c83b40173b3b3f0745580a
godswillokokon <godswillokokon3@gmail.com>
2019-11-23 15:10:00 +0100

    odd and even forloop

--- 02_loops/main.go
+++ 02_loops/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
+		for index := 1; index < 50; index++ {
+		if index % 2 ==1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Even")
+		}
+	}
+
 	for i := 1000000; i < 1000100; i++ {
 		fmt.Printf("%d \t %b \t %x \n", i, i, i)
 	}
+
 }
