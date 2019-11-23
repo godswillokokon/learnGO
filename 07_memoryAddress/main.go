4938b8f81c31bed4900f17073e3811aa77df322d
godswillokokon <godswillokokon3@gmail.com>
2019-11-23 07:49:27 +0100

    got data from keyboard

--- 07_memoryAddress/main.go
+++ 07_memoryAddress/main.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+const metersToYards float64 = 1.09861
+
 func main() {
-	a := 22
-	fmt.Println("a value is ", a)
-	fmt.Println("a memory address is ", &a)
+
+	var meters float64
+	fmt.Print("Enter meters swam: ")
+	fmt.Scan(&meters)
+	yards := meters - metersToYards
+	fmt.Println(meters, "meters is", yards, "yards")
+
+	// a := 22
+	// b := 1
+	// c := 20
+	// fmt.Println("a value is ", a)
+	// fmt.Println("a memory address is ", &a)
+	// fmt.Printf("%d \n", &a)
+	// fmt.Printf("%d \n", &b)
+	// fmt.Printf("%d \n", &c)
+
 }
