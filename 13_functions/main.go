38d0cfe1ca766644da1617023aff2005e6f4aa4d
godswillokokon <godswillokokon3@gmail.com>
2019-11-26 12:20:23 +0100

    mulitple returns

--- 13_functions/main.go
+++ 13_functions/main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	s := greet("Bassey ", "Okon")
 	fmt.Println(s)
+	fmt.Println(greets(" Godswill ", " Okokon "))
 }
 
 func greet(fname, lname string) string {
 	return fmt.Sprint(fname, lname)
+}
+
+func greets(fname, lname string) (string, string) {
+	return fmt.Sprint(fname, lname), fmt.Sprint(lname, fname)
 }
