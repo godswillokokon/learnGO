80c53298dc4ccdd619c23772861ad5447b07e104
godswillokokon <godswillokokon3@gmail.com>
2019-12-16 21:31:52 +0100

    refer type with slice of string

---
+++ 17_referenceTypes/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make([]string, 1, 25)
+	fmt.Println(m)
+	changeMe(m)
+	fmt.Println(m)
+}
+
+func changeMe(z []string) {
+	z[0] = "Mofo"
+	fmt.Println(z)
+}
